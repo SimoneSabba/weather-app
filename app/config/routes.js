1da37bbbcdb1bf27a23d81e55934610cca47e9f9
Simone Sabbatini <sabbatini.simone@gmail.com>
2016-10-07 17:15:18 +0100

    renamed containers

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Router, Route, Link, hashHistory, IndexRoute } from 'react-router';
-import MainComponent from '../containers/main'
-import HomeContainer from '../containers/home'
+import MainComponent from '../containers/MainContainer'
+import HomeContainer from '../containers/HomeContainer'
 
 var routes = (
 	<Router history={hashHistory}>
