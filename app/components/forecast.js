7a9916c50ff9d32661003f988a71b84da93ed352
Simone Sabbatini <sabbatini.simone@gmail.com>
2016-10-08 18:02:29 +0100

    added forecast component

---
+++ app/components/forecast.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Forecast(props) {
+  return (
+    <div>My comp</div>
+  )
+}
+
+export default Forecast;
---
+++ app/containers/ForecastContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Forecast from '../components/forecast';
+
+var ForecastContainer = React.createClass({
+	render: function() {
+		return (
+			<Forecast /> 
+	  	);
+	}
+});
+
+export default ForecastContainer;
