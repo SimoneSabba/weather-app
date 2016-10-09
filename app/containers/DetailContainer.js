457cd7b75f843f3590b4e1dfd118d472922af77d
Simone Sabbatini <sabbatini.simone@gmail.com>
2016-10-09 19:59:26 +0100

    added detailContainer component

---
+++ app/containers/DetailContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Detail from '../components/detail';
+
+var DetailContainer = React.createClass({
+  render: function() {
+    
+    return (
+      <Detail />
+    );
+  }
+
+});
+
+export default DetailContainer;
